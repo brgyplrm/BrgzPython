6ee6a1afde44b666013cf4f28d8e60a74aef6d8c
brgyplrm <borgypalermo@gmail.com>
2025-03-18 21:17:19 +0800

    Update Data types.py

--- Lesson 3-1/Data types.py
+++ Lesson 3-1/Data types.py
@@ -49,7 +49,6 @@ y = 12E4
 z = -87.7e100
 
 print(type(x))
-print(z)
 print(type(y))
 print(type(z))
 
@@ -107,5 +106,32 @@ x = memoryview(bytes(5))
 #NoneType
 x = None	
 
+#conversion 
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# random number
+import random
+
+print(random.randrange(1, 100000))
 
 
