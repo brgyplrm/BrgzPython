c8836ef4147554da5dfb0105e2c7599be52ff556
brgyplrm <borgypalermo@gmail.com>
2025-03-18 20:06:40 +0800

    f

---
+++ Lesson 3-1/Printing-Output.py
@@ -0,0 +1,21 @@
+# Printing
+
+x = "Python is awesome"
+print(x)
+
+# printing can be separate by comma
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+# can also use "+"
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+# you can't print int and string at the same time 
+
+
---
+++ Lesson 3-1/Variables.py
@@ -0,0 +1,38 @@
+# Variables 
+
+x = 5 # this is type int
+y = "John" # this is type str
+
+# Casting
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+# Get the type of data type
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+# Case sensitive
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+#Legal Variables names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Illegal Variable names 
+""" 
+    2myvar = "John"
+    my-var = "John"
+    my var = "John" 
+"""
+
